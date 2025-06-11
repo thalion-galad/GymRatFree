95acdf594ad638b02c2fe127c63373276ff1360a
jose <jvasquezc23_1@unc.edu.pe>
2025-06-11 13:21:56 -0500

    Creacion del proyeccto

---
+++ paginaPrincipla.js
@@ -0,0 +1,6 @@
+function calcularProte{
+    const sexo = document.getElementById("sexo").value;
+    const actividad = document.getElementById("actividad").value;
+    const objetivo = document.getElementById("objetivo").value;
+    let peso 
+}
